32068198639292e43a5db4e71dc277cbabe40eb5
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-03-25 18:30:14 +0100

    ADD bonus esercizio 3 + correzione bonus es. 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,23 +10,31 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
-console.log(teachers);
+
+const reversedTeachers = teachers.toReversed();
+console.log(reversedTeachers);
+  // usato .toreversed al posto di .reverse per lasciare inalterata la lista 
+  // per i restanti esercizi
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
 
 for (let i = 0; i < teachers.length; i++) {
 
     const current_teacher = teachers[i];
 
     if (current_teacher.length >= 5) {
-        console.log(current_teacher);
+        longNames.push(current_teacher)
     }
 }
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(1, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
